30d9421ff5583aac1568461d682ea58a3528b702
Benjamin Moniotte <benjamin.moniotte@gmail.com>
2023-06-12 17:04:46 +0200

    Just simple calculation with fixed values

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! I'm Ben's amazing calculator console app!");
+int a = 20;
+int b = 40;
+int c = a + b;
+Console.WriteLine(c);
+Console.ReadKey();
